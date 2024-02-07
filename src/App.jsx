c0b11b0812bd8dd8e11cc38b21b1282025e2ebb2
jitendra <jiten.cs2010@gmail.com>
2024-02-07 17:55:36 +0000

    search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,72 @@ import { useRef, useState } from "react";
 import "./styles.css";
 import Todolist from "./Todolist";
 
+const data = ["patna", "Punjab", "Pune", "delhi", "bangal"];
+
+function Search() {
+  // let refValue = React.createRef();
+  let refValue = useRef("");
+  const [searchData, setSearchData] = React.useState([]);
+  const onChangeText = (e) => {
+    const data1 = data.filter((a) => {
+      if (e.target.value === "") return;
+      return a.indexOf(e.target.value) === 0;
+    });
+    console.log(data1);
+    setSearchData(data1);
+  };
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <input type="text" onChange={(e) => onChangeText(e)} refs={refValue} />
+      {searchData &&
+        searchData.length > 0 &&
+        searchData.map((a) => {
+          return <div>{a}</div>;
+        })}
+    </div>
+  );
+}
+
 export default function App() {
+  const [selectedMenu, setSelectedMenu] = useState("Todo");
+  const onClickMenu = (text) => {
+    setSelectedMenu(text);
+  };
   return (
     <div className="App">
       <nav>
         <ul>
           <li>
-            <a href="/">Todo</a>
+            <a href="#" onClick={() => onClickMenu("Todo")}>
+              Todo
+            </a>
           </li>
           <li>
-            <a href="/">TicTacToe</a>
+            <a href="#" onClick={() => onClickMenu("TicTacToe")}>
+              TicTacToe
+            </a>
           </li>
           <li>
-            <a href="/">Search</a>
+            <a href="#" onClick={() => onClickMenu("Search")}>
+              Search
+            </a>
           </li>
           <li>
-            <a href="/">Home</a>
+            <a href="#" onClick={() => onClickMenu("Home")}>
+              Home
+            </a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a href="#" onClick={() => onClickMenu("About")}>
+              About
+            </a>
           </li>
         </ul>
       </nav>
-      <Todolist />
+      {selectedMenu === "Todo" && <Todolist />}
+      {selectedMenu === "Search" && <Search />}
     </div>
   );
 }
---
+++ src/Search.jsx
@@ -0,0 +1,30 @@
+import { useRef, useState } from "react";
+import "./styles.css";
+
+const data = ["patna", "Punjab", "Pune", "delhi", "bangal"];
+
+export default function Search() {
+  // let refValue = React.createRef();
+  let refValue = useRef("");
+  const [searchData, setSearchData] = React.useState([]);
+  const onChangeText = (e) => {
+    const data1 = data.filter((a) => {
+      if (e.target.value === "") return;
+      return a.indexOf(e.target.value) === 0;
+    });
+    console.log(data1);
+    setSearchData(data1);
+  };
+  return (
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <input type="text" onChange={(e) => onChangeText(e)} refs={refValue} />
+      {searchData &&
+        searchData.length > 0 &&
+        searchData.map((a) => {
+          return <div>{a}</div>;
+        })}
+    </div>
+  );
+}
