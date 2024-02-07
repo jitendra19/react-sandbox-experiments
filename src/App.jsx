5306536f7ddb2ba931c8e8e9889ba4dbd2029d92
jitendra <jiten.cs2010@gmail.com>
2024-02-07 16:18:15 +0000

    added nav menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,25 @@ import Todolist from "./Todolist";
 export default function App() {
   return (
     <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Todo</a>
+          </li>
+          <li>
+            <a href="/">TicTacToe</a>
+          </li>
+          <li>
+            <a href="/">Search</a>
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
       <Todolist />
     </div>
   );
